f383d8c0f7cf0ad70555f7b7b86e84fb6368d4f3
Soeren Kroell <soeren@time2tri.me>
2018-03-26 10:24:49 +0200

    [TASK] Test import.py updated

--- import.py
+++ import.py
@@ -6,48 +6,43 @@ import datetime
 
 now = datetime.datetime.now()
 
-# if(len(sys.argv)){
-#     fileName = sys.argv[0];
-# } else{
-#     fileName = '/Users/soerenkroell/Desktop/19016651587_mo_swim_aber_ohne_session.fit';
-# }
-
+fileName = False
 
 if len(sys.argv) >= 2:
      fileName = sys.argv[1]
-else:
-     fileName = '/Users/soerenkroell/Desktop/19016651587_mo_swim_aber_ohne_session.fit'
-
-print
-print "Start " + now.strftime("%H:%M:%S")
-print
-
-print "importing file %s"%(fileName)
 
 if len(sys.argv) >= 3:
      fieldName = sys.argv[2]
 else:
      fieldName = 'session'
 
-fitfile = FitFile(fileName)
+if fileName:
+     print "importing file %s" % (fileName)
+     print
+     print "Start " + now.strftime("%H:%M:%S")
+     print
 
-# Get all data messages that are of type record
-for record in fitfile.get_messages(fieldName):
+     fitfile = FitFile(fileName)
 
-    # Go through all the data entries in this record
-    for record_data in record:
+     # Get all data messages that are of type record
+     for record in fitfile.get_messages(fieldName):
 
-        # Print the records name and value (and units if it has any)
-        if record_data.units:
-            print " * %s: %s %s" % (
-                record_data.name, record_data.value, record_data.units,
-            )
-        else:
-            print " * %s: %s" % (record_data.name, record_data.value)
-    print
+          # Go through all the data entries in this record
+          for record_data in record:
 
-now = datetime.datetime.now()
+               # Print the records name and value (and units if it has any)
+               if record_data.units:
+                    print " * %s: %s %s" % (
+                         record_data.name, record_data.value, record_data.units,
+                    )
+               else:
+                    print " * %s: %s" % (record_data.name, record_data.value)
+          print
 
-print
-print"End " + now.strftime("%H:%M:%S")
-print
+     now = datetime.datetime.now()
+
+     print
+     print "End " + now.strftime("%H:%M:%S")
+     print
+else:
+     print "Missing filname"
