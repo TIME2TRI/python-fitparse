45c1b04af916310e5a888764d822bb639ce91b66
Soeren Kroell <soeren@time2tri.me>
2018-03-26 10:43:33 +0200

    [TASK] Test import.py updated

--- import.py
+++ import.py
@@ -1,48 +1,56 @@
 #!/usr/bin/python
 
 from fitparse import FitFile
+
 import sys
 import datetime
+import os
+import json
 
-now = datetime.datetime.now()
-
-fileName = False
-
-if len(sys.argv) >= 2:
-     fileName = sys.argv[1]
-
-if len(sys.argv) >= 3:
-     fieldName = sys.argv[2]
-else:
-     fieldName = 'session'
-
-if fileName:
-     print "importing file %s" % (fileName)
-     print
-     print "Start " + now.strftime("%H:%M:%S")
-     print
-
-     fitfile = FitFile(fileName)
-
-     # Get all data messages that are of type record
-     for record in fitfile.get_messages(fieldName):
-
-          # Go through all the data entries in this record
-          for record_data in record:
-
-               # Print the records name and value (and units if it has any)
-               if record_data.units:
-                    print " * %s: %s %s" % (
-                         record_data.name, record_data.value, record_data.units,
-                    )
-               else:
-                    print " * %s: %s" % (record_data.name, record_data.value)
-          print
-
+def importFit(fileName, fieldName):
      now = datetime.datetime.now()
 
-     print
-     print "End " + now.strftime("%H:%M:%S")
-     print
-else:
-     print "Missing filname"
+     if fileName:
+          print "importing file %s" % (fileName)
+          print
+          print "Start " + now.strftime("%H:%M:%S")
+          print
+
+          fitfile = FitFile(fileName)
+
+          # Get all data messages that are of type record
+          for record in fitfile.get_messages(fieldName):
+
+               # Go through all the data entries in this record
+               for record_data in record:
+
+                    # Print the records name and value (and units if it has any)
+                    if record_data.units:
+                         print " * %s: %s %s" % (
+                         record_data.name, record_data.value, record_data.units,
+                         )
+                    else:
+                         print " * %s: %s" % (record_data.name, record_data.value)
+               print
+
+          now = datetime.datetime.now()
+
+          print
+          print "End " + now.strftime("%H:%M:%S")
+          print
+     else:
+          print "Missing filname"
+
+
+if __name__ == "__main__":
+     fileName = False
+
+     if len(sys.argv) >= 2:
+          fileName = sys.argv[1]
+
+     if len(sys.argv) >= 3:
+          fieldName = sys.argv[2]
+     else:
+          fieldName = 'session'
+
+     print importFit(fileName, fieldName)
